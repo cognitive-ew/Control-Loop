772fa524eea452367f2a90128af1e83364e3a69a
cognitive-ew <kzhaigh@gmail.com>
2025-07-28 10:17:51 -0500

    Add files via upload

--- PUK_kernel.m
+++ PUK_kernel.m
@@ -1,4 +1,10 @@
 %% Pearson VII kernel
+%
+% Ustun, Melssen, and Buydens, "Facilitating the application of Support Vector
+% Regression by using a Universal Pearson VII Function Based Kernel," Chemometrics and
+% Intelligent Laboratory Systems, Vol. 81, No. 1, 2006.
+% DOI: 10.1016/j.chemolab.2005.09.003.
+					      
 function K = PUK_kernel(U, V)
     sigma=1.0;
     omega=1.0;
